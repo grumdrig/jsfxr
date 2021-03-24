08312d119f4b5e38ba94e4740ec0d339c7ff5a22
Chris McCormick <chris@mccormick.cx>
2021-03-24 15:33:51 +0800

    Moving stuff around.

--- sfxr.js
+++ sfxr.js
@@ -350,31 +350,34 @@ Params.prototype.blipSelect = function () {
   return this;
 }
 
-Params.prototype.mutate = function () {
-  if (rnd(1)) this.p_base_freq += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_freq_ramp += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_freq_dramp += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_duty += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_duty_ramp += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_vib_strength += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_vib_speed += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_vib_delay += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_env_attack += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_env_sustain += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_env_decay += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_env_punch += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_lpf_resonance += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_lpf_freq += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_lpf_ramp += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_hpf_freq += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_hpf_ramp += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_pha_offset += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_pha_ramp += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_repeat_speed += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_arp_speed += frnd(0.1) - 0.05;
-  if (rnd(1)) this.p_arp_mod += frnd(0.1) - 0.05;
+Params.prototype.synth = function () {
+  this.wave_type = rnd(1);
+  this.p_base_freq = rnd(1) ? 0.2477 : 0.1737;
+  this.p_env_attack = rnd(4) > 3 ? frnd(0.5) : 0;
+  this.p_env_sustain = frnd(1);
+  this.p_env_punch = frnd(1);
+  this.p_env_decay = frnd(0.9) + 0.1;
+  this.p_arp_mod = [0, 0, 0, 0, -0.3162, 0.7454, 0.7454][rnd(6)];
+  this.p_arp_speed = frnd(0.5) + 0.4;
+  this.p_duty = frnd(1);
+  this.p_duty_ramp = rnd(2) == 2 ? frnd(1) : 0;
+  this.p_lpf_freq = [1, frnd(1) * frnd(1)][rnd(1)];
+  this.p_lpf_ramp = rndr(-1, 1);
+  this.p_lpf_resonance = frnd(1);
+  this.p_hpf_freq = rnd(3) == 3 ? frnd(1) : 0;
+  this.p_hpf_ramp = rnd(3) == 3 ? frnd(1) : 0;
+  return this;
 }
 
+Params.prototype.tone = function () {
+  this.wave_type = SINE;
+  this.p_base_freq = 0.35173364; // 440 Hz
+  this.p_env_attack = 0;
+  this.p_env_sustain = 0.6641; // 1 sec
+  this.p_env_decay = 0;
+  this.p_env_punch = 0;
+  return this;
+}
 
 Params.prototype.random = function () {
   this.wave_type = rnd(3);
@@ -416,32 +419,29 @@ Params.prototype.random = function () {
   return this;
 }
 
-Params.prototype.synth = function () {
-  this.wave_type = rnd(1);
-  this.p_base_freq = rnd(1) ? 0.2477 : 0.1737;
-  this.p_env_attack = rnd(4) > 3 ? frnd(0.5) : 0;
-  this.p_env_sustain = frnd(1);
-  this.p_env_punch = frnd(1);
-  this.p_env_decay = frnd(0.9) + 0.1;
-  this.p_arp_mod = [0, 0, 0, 0, -0.3162, 0.7454, 0.7454][rnd(6)];
-  this.p_arp_speed = frnd(0.5) + 0.4;
-  this.p_duty = frnd(1);
-  this.p_duty_ramp = rnd(2) == 2 ? frnd(1) : 0;
-  this.p_lpf_freq = [1, frnd(1) * frnd(1)][rnd(1)];
-  this.p_lpf_ramp = rndr(-1, 1);
-  this.p_lpf_resonance = frnd(1);
-  this.p_hpf_freq = rnd(3) == 3 ? frnd(1) : 0;
-  this.p_hpf_ramp = rnd(3) == 3 ? frnd(1) : 0;
-  return this;
-}
-
-Params.prototype.tone = function () {
-  this.wave_type = SINE;
-  this.p_base_freq = 0.35173364; // 440 Hz
-  this.p_env_attack = 0;
-  this.p_env_sustain = 0.6641; // 1 sec
-  this.p_env_decay = 0;
-  this.p_env_punch = 0;
+Params.prototype.mutate = function () {
+  if (rnd(1)) this.p_base_freq += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_freq_ramp += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_freq_dramp += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_duty += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_duty_ramp += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_vib_strength += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_vib_speed += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_vib_delay += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_env_attack += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_env_sustain += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_env_decay += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_env_punch += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_lpf_resonance += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_lpf_freq += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_lpf_ramp += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_hpf_freq += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_hpf_ramp += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_pha_offset += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_pha_ramp += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_repeat_speed += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_arp_speed += frnd(0.1) - 0.05;
+  if (rnd(1)) this.p_arp_mod += frnd(0.1) - 0.05;
   return this;
 }
 
